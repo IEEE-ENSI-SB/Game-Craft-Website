e528871d57e426cfc6a5248f355c155cc993511f
Aloui Aymen <153553749+AymenENSI@users.noreply.github.com>
2024-04-05 21:18:38 +0100

    il y a des modifications que j'ai fait etque j'ai pas commit hier

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -29,30 +29,35 @@ function ContactUs() {
   return (
     <div class="bg-gradient-to-br from-purple-950 via-violet-500 to-cyan-500   bg-dvh h-full">
 
-      <div className='text-white text-2xl font-stalinist mx-4 pt-2'>ContactUs</div>
+      <div className='text-white text-4xl font-stalinist mx-4 pt-2'>ContactUs</div>
 
-          <div className='border-[1px] border-rgb(246, 161, 53) '>
-      <div className="border-[1px] border-rgb(246, 161, 53) my-[15px] w-[70rem] mx-auto rounded-[8px] flex shadow-lg shadow-white hover:shadow-violet-400 ">
-        <form onSubmit={handleSubmit} className="mt-5 ml-[20px] mx-[10rem] border-[1px] border-rgb(246, 161, 53) w-[32.8rem] h-[40rem] rounded-lg ">
-          <div className='border-[1px] border-rgb(246, 161, 53) h-[2.8rem] w-[32.8rem] flex justify-between'>
-            <input type="text" name='firstname' id='firstname' onChange={(e)=> setfirstname(e.target.value)} className='rounded-[5px] w-64 h-[2.8rem] outline-none border-0 pl-[8px] text-[0.9rem] ' placeholder='Last Name' required/>
-            <input type="text" name='lastname' id='lastname' onChange={(e)=> setlastname(e.target.value)} className='rounded-[5px] w-64 h-[2.8rem] outline-none border-0 pl-[8px] text-[0.9rem]' placeholder='First Name' required/>
+        <div>
+          <div className=" my-[15px] w-[70rem] mx-auto rounded-[8px] flex shadow-2xl shadow-white hover:shadow-purple-400 backdrop-grayscale backdrop-opacity-40">
+          
+          <form onSubmit={handleSubmit} className=" mt-5 ml-[20px] mx-[10rem]  w-[32.8rem] h-[40rem] rounded-lg ">
+            <p className=" text-white text-3xl font-semibold">Let's Connect</p>
+            <p className="text-white mt-[4px] pl-1 text-[1.14rem] ">Join us in connecting the dots of our gaming universe! Reach out and let's weave together the magic of collaboration to create exceptional experiences.</p>
+            <div className=' h-[2.8rem] w-[32.8rem] flex justify-between mt-5'>
+              <input type="text" name='firstname' id='firstname' onChange={(e)=> setfirstname(e.target.value)} className='border-[1px] border-indigo-900 rounded-[5px] w-64 h-[2.8rem] outline-none border-0 pl-[8px] text-[0.9rem] bg-transparent text-white ' placeholder='Last Name' required/>
+              <input type="text" name='lastname' id='lastname' onChange={(e)=> setlastname(e.target.value)} className='border-[1px] border-indigo-800 rounded-[5px] w-64 h-[2.8rem] outline-none border-0 pl-[8px] text-[0.9rem] bg-transparent text-white '    placeholder='First Name' required/>
+            </div>
+            <input type="email" id='email' onChange={(e)=> setemail(e.target.value)} className="border-[1px] border-indigo-900 rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] outline-none border-0 pl-[8px] text-[0.9rem] bg-transparent text-white" placeholder='Email' required/>
+            <input type="tel" id='tel' onChange={(e)=> settel(e.target.value)} className="border-[1px] border-indigo-800 rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] outline-none border-0 pl-[8px] text-[0.9rem] bg-transparent text-white" placeholder='Tel' required /> 
+            <textarea name="message" id="message" onChange={(e)=>setmessage(e.target.value)} cols="30" rows="10" className='border-[1px] border-indigo-800 mt-[0.9rem] rounded-[5px] w-[32.8rem] outline-none  pl-[8px] pt-[6px]  resize-none text-[0.9rem] bg-transparent text-white  ' placeholder='Message' required></textarea>
+            <button type='submit' className="rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] bg-gradient-to-l from-purple-700 to-indigo-600 text-white text-lg"> Send it</button>
+          </form>
+
+          <div className="w-[70rem] mt-5 mr-5 mb-10 rounded-[5px] relative" style={{ marginLeft: '-140px' }}>
+            <div className="absolute inset-0 bg-blue-200 opacity-50 rounded-[5px]"></div>
+            <img src={ENSIMapsImage} alt="Ensi maps" className="w-full h-full rounded-[5px]" />
           </div>
-          <input type="email" id='email' onChange={(e)=> setemail(e.target.value)} className="rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] outline-none border-0 pl-[8px] text-[0.9rem]" placeholder='Email' required/>
-          <input type="tel" id='tel' onChange={(e)=> settel(e.target.value)} className="rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] outline-none border-0 pl-[8px] text-[0.9rem]" placeholder='Tel' required /> 
-          <textarea name="message" id="message" onChange={(e)=>setmessage(e.target.value)} cols="30" rows="10" className='mt-[0.9rem] rounded-[5px] w-[32.8rem] outline-none  pl-[8px] pt-[6px] h-80 resize-none text-[0.9rem] ' placeholder='Message' required></textarea>
-          <button type='submit' className="rounded-[5px] w-full h-[2.8rem] mt-[0.9rem] border bg-gradient-to-l from-purple-700 to-indigo-600 text-white text-lg"> Send it</button>
-        </form>
 
-        <div className=" w-[70rem]  mt-5 mr-5 mb-10 rounded-[5px]  " style={{ marginLeft: '-140px' }}>
-          <img src={ENSIMapsImage} alt="Ensi maps" className="w-[100%] h-[100%]  rounded-[5px]" />
-        </div>
+         </div>
       </div>
-    </div>
 
     </div>
     
-  )
+  ) 
 }
 
 export default ContactUs
