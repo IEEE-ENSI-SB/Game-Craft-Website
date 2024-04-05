a2299811e05ba961f0e4e279941459d147575463
ahmedkamoun2003 <ahmed.kamoun@ensi-uma.tn>
2024-04-05 13:54:40 +0100

    news3

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import News1 from '../assets/news/new1.png'
 import News2 from '../assets/news/new2.png'
+import News3 from '../assets/news/new3.png'
 import New from './ahmed/New'
 function News() {
   return (
     <div className='flex flex-wrap min-h-svh'>
-      <New src={News1} />
+      <New src={News1}/>
       <New src={News2}/>
+      <New src={News3}/>
     </div>
   )
 }
